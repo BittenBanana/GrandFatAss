3468be5e6dcb3d9af159cc4ff027a2a25a17df7e
Tomek <tom.gabryelewicz@gmail.com>
2017-12-04 21:01:59 +0100

    Added wasted delay, fixed timescale, added exit and restart to wasted, 5sec light traffic

--- Assets/PlayerDeathController.cs
+++ Assets/PlayerDeathController.cs
@@ -1,20 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerDeathController : MonoBehaviour {
+public class PlayerDeathController : MonoBehaviour
+{
 
     public GameObject WastedObject;
     public GameObject PoliceEffect;
     public GameObject EneFatGUI;
     public GameObject Player;
 
+    bool isDead = false;
+    float timer = 0;
+
+
+    private void Update()
+    {
+        if (isDead)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 1.0f)
+            {
+                WastedObject.SetActive(true);
+                PoliceEffect.SetActive(false);
+                EneFatGUI.SetActive(false);
+                Player.GetComponent<PlayerController>().enabled = false;
+                GetComponent<GameTimer>().CanCount = false;
+            }
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                Time.timeScale = 1f;
+                SceneManager.LoadScene("New Scene");
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Time.timeScale = 1f;
+                SceneManager.LoadScene("layout");
+            }
+        }
+    }
     public void Wasted()
     {
-        WastedObject.SetActive(true);
-        PoliceEffect.SetActive(false);
-        EneFatGUI.SetActive(false);
-        Player.GetComponent<PlayerController>().enabled = false;
-        GetComponent<GameTimer>().CanCount = false;
+        isDead = true;
+
     }
 }
